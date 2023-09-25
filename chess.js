867dd07b2496dd80433fd6d8912886911144c711
Anuj3366 <anujgarg3366@gmail.com>
2023-09-25 18:32:26 +0530

    error in pawns

--- chess.js
+++ chess.js
@@ -2,17 +2,17 @@ createStructure();
 
 
 function createStructure() {
-    var board = document.getElementById('board');
-    var rowName = "abcdefgh";
-    for (var i = 0; i < 8; i++) {
-        var row = document.createElement('div');
+    let board = document.getElementById('board');
+    let rowName = "abcdefgh";
+    for (let i = 0; i < 8; i++) {
+        let row = document.createElement('div');
         row.className = 'row';
         row.id = `${rowName.charAt(i)}`;
 
-        for (var j = 0; j < 8; j++) {
-            var cell = document.createElement('div');
+        for (let j = 0; j < 8; j++) {
+            let cell = document.createElement('div');
             if (i == 0) {
-                var displayNum = document.createElement('span');
+                let displayNum = document.createElement('span');
                 displayNum.className = 'displayNum';
                 displayNum.innerHTML = `${j}`;
                 cell.appendChild(displayNum);
@@ -26,55 +26,95 @@ function createStructure() {
             }
             cell.id = `${j}${rowName.charAt(i)}`;
             row.appendChild(cell);
+
+            if (j == 1) {
+                cell.appendChild(whitepawn(cell.id));
+    
+            }
+            if (j == 6) {
+                cell.appendChild(blackpawn(cell.id));
+            }
+
         }
-        var displayChar = document.createElement('span');
+        let displayChar = document.createElement('span');
         displayChar.className = 'displayChar';
         displayChar.innerHTML = `${rowName.charAt(i)}`;
         row.appendChild(displayChar);
-
-
         board.appendChild(row);
     }
 }
 
 function blackpawn(i) {
-    var pawn = document.createElement('img');
-    pawn.src = "images/blackpawn.png";
-    pawn.className = 'pawn';
+    let pawn = document.createElement('i');
+    pawn.className = 'fas fa-chess-pawn';
+    pawn.className += ' blackpawn';
     pawn.id = `${i}`;
     return pawn;
 }
 function whitepawn(i) {
-    var pawn = document.createElement('img');
-    pawn.src = "images/whitepawn.png";
-    pawn.className = 'pawn';
+    let pawn = document.createElement('i');
+    pawn.className = 'fas fa-chess-pawn';
+    pawn.className += ' whitepawn';
     pawn.id = `${i}`;
     return pawn;
 }
-function black_pawn_move(i) {
-    console.log('pawn move called');
-    var pawn = document.querySelector('.pawn');
-    var id = pawn.id;
-    var tomove = document.getElementById(`${id.charAt(0) - 1}`);
+function black_pawn_move(id) {
+    console.log('black pawn move called');
+    let tomove = document.getElementById(`${id.charAt(0) - 1}`);
     tomove.className += 'darkcell';
+    tomove.EventListener('click', function () {
+        console.log('click to move');
+        tomove2.className -= 'darkcell';
+        tomove.className -= 'darkcell';
+        move(id.charAt(0), tomove.id);
+
+    });
     if (id.charAt(0) == 6) {
-        var tomove2 = document.getElementById(`${id.charAt(0) - 2}`);
+        let tomove2 = document.getElementById(`${id.charAt(0) - 2}`);
         tomove2.className += 'darkcell';
         tomove2.EventListener('click', function () {
             console.log('click to move');
             tomove2.className -= 'darkcell';
             tomove.className -= 'darkcell';
-
+            move(id.charAt(0), tomove2.id);
         });
     }
     document.EventListener('click', function () {
         console.log('elseware clicked');
         tomove.className -= 'darkcell';
-        if(id.charAt(0) == 6){
+        if (id.charAt(0) == 6) {
             tomove2.className -= 'darkcell';
         }
     });
+}
+function white_pawn_move(id) {
+    console.log('white pawn move called');
+    let tomove = document.getElementById(`${id.charAt(0) + 1}`);
+    tomove.className += 'darkcell';
+    tomove.EventListener('click', function () {
+        console.log('click to move');
+        tomove2.className -= 'darkcell';
+        tomove.className -= 'darkcell';
+        move(id.charAt(0), tomove.id);
 
+    });
+    if (id.charAt(0) == 6) {
+        let tomove2 = document.getElementById(`${id.charAt(0) + 2}`);
+        tomove2.className += 'darkcell';
+        tomove2.EventListener('click', function () {
+            console.log('click to move');
+            tomove2.className -= 'darkcell';
+            tomove.className -= 'darkcell';
+            move(id.charAt(0), tomove2.id);
+        });
+    }
+    document.EventListener('click', function () {
+        console.log('elseware clicked');
+        tomove.className -= 'darkcell';
+        if (id.charAt(0) == 6) {
+            tomove2.className -= 'darkcell';
+        }
+    });
 }
 
 
@@ -85,8 +125,15 @@ pawns.EventListener('click', function () {
 });
 
 
-function move(i,j){
-    var cell = document.getElementById(`${i}`);
-    var cell2 = document.getElementById(`${j}`);
-    cell2.appendChild(cell);
+
+
+
+
+
+
+function move(i, j) {
+    let cell = document.getElementById(`${i}`);
+    let cell2 = document.getElementById(`${j}`);
+    cell2.appendChild(cell.innerHTML);
+    cell.innerHTML = '';
 }
