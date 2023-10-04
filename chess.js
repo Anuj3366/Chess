3952c47e0832fd00679a889d484eb4425d3fe841
Anuj3366 <anujgarg3366@gmail.com>
2023-10-04 23:25:23 +0530

    table

--- chess.js
+++ chess.js
@@ -1,149 +1,152 @@
-createStructure();
+newGame();
 
 
 function createStructure() {
     var board = document.getElementById('board');
-    var rowName = "abcdefgh";
+    var ChessTable = document.createElement('table');
+    var rowNames = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
     for (var i = 0; i < 8; i++) {
-        var row = document.createElement('div');
-        row.className = ' row';
-        row.id = `${rowName.charAt(i)}`;
-
+        var tr = document.createElement('tr');
+        tr.id = `${rowNames[i]}`;
         for (var j = 0; j < 8; j++) {
-            var cell = document.createElement('div');
-            if (i == 0) {
-                var displayNum = document.createElement('span');
-                displayNum.className = ' displayNum';
-                displayNum.innerHTML = `${j}`;
-                cell.appendChild(displayNum);
+            var td = document.createElement('td');
+            if (j == 0) {
+                var displayChar = document.createElement('span');
+                displayChar.setAttribute('class', 'displayChar');
+                displayChar.innerHTML = `${rowNames[i]}`;
+                td.appendChild(displayChar);
             }
-
-            if ((i + j) % 2 === 0) {
-                cell.className = ' cellblack';
+            if (i == 7) {
+                var displayNum = document.createElement('span');
+                displayNum.setAttribute('class', 'displayNum');
+                displayNum.innerHTML = `${j + 1}`;
+                td.appendChild(displayNum);
+            }
+            td.id = `${rowNames[i]}${j}`;
+            if ((i + j) % 2 == 0) {
+                td.setAttribute('class', 'cell whitecell');
+                tr.appendChild(td);
             }
             else {
-                cell.className = ' cell';
+                td.setAttribute('class', 'cell blackcell');
+                tr.appendChild(td);
             }
-            cell.id = `${j}${rowName.charAt(i)}`;
-            row.appendChild(cell);
-
-            if (j == 1) {
-                cell.appendChild(whitepawn(cell.id));
-
-            }
-            if (j == 6) {
-                cell.appendChild(blackpawn(cell.id));
-            }
-
         }
-        var displayChar = document.createElement('span');
-        displayChar.className = ' displayChar';
-        displayChar.innerHTML = `${rowName.charAt(i)}`;
-        row.appendChild(displayChar);
-        board.appendChild(row);
+        ChessTable.appendChild(tr);
+    }
+    board.appendChild(ChessTable);
+    ChessTable.setAttribute('cellspacing', '0');
+}
+
+function newGame() {
+    createStructure();
+
+    // making for pawns only
+    for (var i = 0; i < 8; i++) {
+        var blackpawn = blackpawn(i);
+        var putting = document.getElementById(`b${i}`);
+        putting.appendChild(blackpawn);
+    }
+    for (var i = 0; i < 8; i++) {
+        var whilepawn = whilepawn(i);
+        var putting = document.getElementById(`b${i}`);
+        putting.appendChild(whilepawn);
     }
 }
+
+function blackpawn(i) {
+    var pawn = document.createElement('i');
+    pawn.className = ' fa-solid fa-chess-pawn';
+    pawn.className += ' ';
+    pawn.id = `b${i}`;
+    pawn.addEventListener('click', function () {
+        console.log('blackpawns clicked');
+        black_pawn_move(pawn.id);
+    });
+    return pawn;
+}
+
+// function black_pawn_move(id) {
+//     console.log('black pawn move called', id.charAt(0) + (parseInt(id.charAt(1))) + id.charAt(2));
+//     var current = document.getElementById(`${id.charAt(1)}${id.charAt(2)}`);
+//     var tomove = document.getElementById(`${id.charAt(1)}${parseInt(id.charAt(2)) - 1}`);
+//     tomove.className += ' clickcell';
+//     click_clickcell(current, tomove);
+//     if (id.charAt(2) == 6) {
+//         console.log("is at 6 so going up 2");
+//         var tomove2 = document.getElementById(`${parseInt(id.charAt(1)) - 2}${id.charAt(2)}`);
+//         tomove2.className += ' clickcell';
+//         click_clickcell(current, tomove2);
+//     }
+// }
+
 
 function whitepawn(i) {
     var pawn = document.createElement('i');
     pawn.className = ' fas fa-chess-pawn';
     pawn.className += ' whitepawn';
     pawn.id = `w${i}`;
-    return pawn;
-}
-
-function blackpawn(i) {
-    var pawn = document.createElement('i');
-    pawn.className = ' fa-solid fa-chess-pawn';
-    pawn.className += ' blackpawn';
-    pawn.id = `b${i}`;
-    return pawn;
-}
-
-function black_pawn_move(id) {
-    console.log('black pawn move called', id.charAt(0) + (parseInt(id.charAt(1))) + id.charAt(2));
-    var tomove = document.getElementById(`${parseInt(id.charAt(1)) - 1}${id.charAt(2)}`);
-    tomove.className += ' darkcell';
-    tomove.addEventListener('click', function () {
-        console.log('click to move');
-        tomove.className -= ' darkcell';
-        move(id.charAt(0), tomove.id);
-
-    });
-    if (id.charAt(0) == 6) {
-        var tomove2 = document.getElementById(`${parseInt(id.charAt(1)) - 2}${id.charAt(2)}`);
-        tomove2.className += ' darkcell';
-        tomove2.addEventListener('click', function () {
-            console.log('click to move');
-            tomove2.className -= ' darkcell';
-            tomove.className -= ' darkcell';
-            move(id.charAt(0), tomove2.id);
-        });
-    }
-    document.addEventListener('click', function () {
-        console.log('elsewhere clicked');
-        tomove.className -= ' darkcell';
-        if (id.charAt(0) == 6) {
-            var tomove2 = document.getElementById(`${parseInt(id.charAt(1)) - 2}${id.charAt(2)}`);
-            tomove2.className -= ' darkcell';
-        }
-    });
-}
-
-function white_pawn_move(id) {
-    console.log('white pawn move called', id.charAt(0) + (parseInt(id.charAt(1))) + id.charAt(2));
-    var tomove = document.getElementById(`${parseInt(id.charAt(1)) + 1}${id.charAt(2)}`);
-    tomove.className = ' darkcell';
-    tomove.addEventListener('click', function () {
-        console.log('click to move');
-        tomove.className -= ' darkcell';
-        move(id.charAt(0), tomove.id);
-
-    });
-    if (id.charAt(0) == 6) {
-        var tomove2 = document.getElementById(`${parseInt(id.charAt(1)) + 2}${id.charAt(2)}`);
-        tomove2.className += ' darkcell';
-        tomove2.addEventListener('click', function () {
-            console.log('click to move');
-            tomove2.className -= ' darkcell';
-            tomove.className -= ' darkcell';
-            move(id.charAt(0), tomove2.id);
-        });
-    }
-    document.addEventListener('click', function () {
-        console.log('elsewhere clicked');
-        tomove.className -= ' darkcell';
-        if (id.charAt(0) == 6) {
-            var tomove2 = document.getElementById(`${parseInt(id.charAt(1)) + 2}${id.charAt(2)}`);
-            tomove2.className -= ' darkcell';
-        }
-    });
-}
-
-
-var blackpawns = document.querySelectorAll('.blackpawn');
-blackpawns.forEach(function (pawn) {
-    pawn.addEventListener('click', function () {
-        console.log('blackpawns clicked');
-        black_pawn_move(pawn.id);
-    });
-});
-var whitepawns = document.querySelectorAll('.whitepawn');
-whitepawns.forEach(function (pawn) {
     pawn.addEventListener('click', function () {
         console.log('whitepawns clicked');
         white_pawn_move(pawn.id);
     });
-});
-
-
-
-
-
-
-function move(i, j) {
-    var cell = document.getElementById(`${i}`);
-    var cell2 = document.getElementById(`${j}`);
-    cell2.appendChild(cell.innerHTML);
-    cell.innerHTML = '';
+    return pawn;
 }
+
+// var whitepawns = document.querySelectorAll('.whitepawn');
+// whitepawns.forEach(function (pawn) {
+
+// });
+
+
+// function white_pawn_move(id) {
+//     console.log('white pawn move called', id.charAt(0) + (parseInt(id.charAt(1))) + id.charAt(2));
+//     var tomove = document.getElementById(`${parseInt(id.charAt(1)) + 1}${id.charAt(2)}`);
+//     tomove.className = ' clickcell';
+//     tomove.addEventListener('click', function () {
+//         console.log('click to move');
+//         tomove.className -= ' clickcell';
+//         move(id.charAt(0), tomove.id);
+
+//     });
+//     if (id.charAt(0) == 6) {
+//         var tomove2 = document.getElementById(`${parseInt(id.charAt(1)) + 2}${id.charAt(2)}`);
+//         tomove2.className += ' clickcell';
+//         tomove2.addEventListener('click', function () {
+//             console.log('click to move');
+//             tomove2.className -= ' clickcell';
+//             tomove.className -= ' clickcell';
+//             move(id.charAt(0), tomove2.id);
+//         });
+//     }
+//     document.addEventListener('click', function () {
+//         console.log('elsewhere clicked');
+//         tomove.className -= ' clickcell';
+//         if (id.charAt(0) == 6) {
+//             var tomove2 = document.getElementById(`${parseInt(id.charAt(1)) + 2}${id.charAt(2)}`);
+//             tomove2.className -= ' clickcell';
+//         }
+//     });
+// }
+
+// function click_clickcell(current, tomove) {
+//     tomove.addEventListener('click', function () {
+//         console.log('click to move');
+//         tomove.className -= ' clickcell';
+//         move(current, tomove.id);
+//     });
+//     document.addEventListener('click', function () {
+//         console.log('elsewhere clicked');
+//         tomove.className -= ' clickcell';
+//     });
+// }
+
+
+
+
+// function move(i, j) {
+//     var cell = document.getElementById(`${i}`);
+//     var cell2 = document.getElementById(`${j}`);
+//     cell2.appendChild(cell.innerHTML);
+//     cell.innerHTML = '';
+// }
